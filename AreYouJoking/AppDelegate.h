913f7b0d83e18a8737f04fbe58119f0b64ccd2c8
BonichSorokin <bonich@MacBook-Pro-Bonich.local>
2015-10-17 14:14:36 +0300

    initialCommit

---
+++ AreYouJoking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AreYouJoking
+//
+//  Created by Bonich on 17.10.15.
+//  Copyright © 2015 Bonich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AreYouJoking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AreYouJoking
+//
+//  Created by Bonich on 17.10.15.
+//  Copyright © 2015 Bonich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
